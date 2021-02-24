05ee61f3d04b4e6a5f3e63854b8e1deddecf0146
Matthew Stanciu <mattbstanciu@gmail.com>
2021-02-24 11:56:32 -0500

    twin peaks!

--- pages/api/set-profile.js
+++ pages/api/set-profile.js
@@ -1,26 +1,26 @@
-const axios = require("axios").default;
+// require('dotenv').config()
+const fs = require('fs')
 import { WebClient } from "@slack/web-api";
 const S1 = require('s1db')
 const db = new S1(process.env.S1_TOKEN)
 
 export default async (req, res) => {
   const client = new WebClient();
-
   
   const context = require.context('../../public/images', true)
   let photos = context.keys()
-  let photo = photos[Math.floor(Math.random() * photos.length)].replace('./', 'https://change-my-pfp.now.sh/images/')
-  const image = await axios.get(photo, {
-    responseType: "arraybuffer",
-  });
-  await db.set('image', photo)
+  let photo = photos[Math.floor(Math.random() * photos.length)].substr(2)
+  console.log('setting profile picture to', photo)
+
+  const image = await fs.readFileSync('./public/images/' + photo)
   const slackRequest = await client.users.setPhoto({
-    image: image.data,
+    image: image,
     token: process.env.SLACK_TOKEN,
-  });
-  const slackRequest1 = await client.users.setPhoto({
-    image: image.data,
-    token: process.env.STU_CO_SLACK_TOKEN,
-  });
-  res.redirect('https://change-my-pfp.now.sh');
+  }).then((r) => {
+    console.log('success')
+    res.status(200).end()
+  }).catch(err => {
+    console.log('error', err)
+    res.status(502).end()
+  })
 };
